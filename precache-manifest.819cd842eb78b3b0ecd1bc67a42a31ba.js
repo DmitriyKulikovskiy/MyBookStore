c4604619c3460df7f60d0c5c2c7fe555ddb0e3d7
DmitriyKulikovskiy <ru.rama@yandex.ru>
2020-09-13 18:59:39 +0300

    Updates

---
+++ precache-manifest.819cd842eb78b3b0ecd1bc67a42a31ba.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1315477019e7fc9300345747bde4a47",
+    "url": "/DmitriyKulikovskiy/MyBookStore/index.html"
+  },
+  {
+    "revision": "2eb429238599c25f2cf6",
+    "url": "/DmitriyKulikovskiy/MyBookStore/static/css/2.8958c31c.chunk.css"
+  },
+  {
+    "revision": "3acdec9f2a708d9fec4f",
+    "url": "/DmitriyKulikovskiy/MyBookStore/static/css/main.11a5de68.chunk.css"
+  },
+  {
+    "revision": "2eb429238599c25f2cf6",
+    "url": "/DmitriyKulikovskiy/MyBookStore/static/js/2.1f0c86de.chunk.js"
+  },
+  {
+    "revision": "bd165ae910f5e1501861ef14c56208ca",
+    "url": "/DmitriyKulikovskiy/MyBookStore/static/js/2.1f0c86de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3acdec9f2a708d9fec4f",
+    "url": "/DmitriyKulikovskiy/MyBookStore/static/js/main.3b18758e.chunk.js"
+  },
+  {
+    "revision": "fb36860a61a4611c3600",
+    "url": "/DmitriyKulikovskiy/MyBookStore/static/js/runtime-main.65d21905.js"
+  },
+  {
+    "revision": "eea89bf97806b781972e4cbde988575c",
+    "url": "/DmitriyKulikovskiy/MyBookStore/static/media/1.eea89bf9.webp"
+  },
+  {
+    "revision": "a3810f981f006a2c7d6c20a8bf99623b",
+    "url": "/DmitriyKulikovskiy/MyBookStore/static/media/2.a3810f98.webp"
+  },
+  {
+    "revision": "0ed089f385eff65af7040f4cb0b9df99",
+    "url": "/DmitriyKulikovskiy/MyBookStore/static/media/3.0ed089f3.jpg"
+  },
+  {
+    "revision": "38da34fd617564e32fc7811459e0ada3",
+    "url": "/DmitriyKulikovskiy/MyBookStore/static/media/4.38da34fd.jpg"
+  },
+  {
+    "revision": "2d8fa631df86612e9ae6ea09ef027f76",
+    "url": "/DmitriyKulikovskiy/MyBookStore/static/media/5.2d8fa631.jpg"
+  },
+  {
+    "revision": "6d6dbd074c545d2642aae54685e74a46",
+    "url": "/DmitriyKulikovskiy/MyBookStore/static/media/6.6d6dbd07.jpg"
+  },
+  {
+    "revision": "c78153467e6bd993226bddb8045903d7",
+    "url": "/DmitriyKulikovskiy/MyBookStore/static/media/7.c7815346.jpg"
+  },
+  {
+    "revision": "104f867a78d593d681f39120d4e33257",
+    "url": "/DmitriyKulikovskiy/MyBookStore/static/media/8.104f867a.jpg"
+  },
+  {
+    "revision": "f3a3761d17f5bdb5169e1bdc071d18e9",
+    "url": "/DmitriyKulikovskiy/MyBookStore/static/media/9.f3a3761d.jpg"
+  },
+  {
+    "revision": "700c4abb9bd63f59d14ec5e7640c08e6",
+    "url": "/DmitriyKulikovskiy/MyBookStore/static/media/about-us.700c4abb.jpg"
+  },
+  {
+    "revision": "7818c8e34b1b37c30f13c9625909622e",
+    "url": "/DmitriyKulikovskiy/MyBookStore/static/media/au_member_1.7818c8e3.jpg"
+  },
+  {
+    "revision": "49afe106b8062038f090ba8d37e600d9",
+    "url": "/DmitriyKulikovskiy/MyBookStore/static/media/au_member_2.49afe106.jpg"
+  },
+  {
+    "revision": "5d8b8a3c579ba0f7374f9ad3f7620fec",
+    "url": "/DmitriyKulikovskiy/MyBookStore/static/media/au_member_3.5d8b8a3c.jpg"
+  },
+  {
+    "revision": "ab4fbb46003a0bd3be8279a4fb996b0c",
+    "url": "/DmitriyKulikovskiy/MyBookStore/static/media/au_member_4.ab4fbb46.jpg"
+  },
+  {
+    "revision": "2fd1a3cdada7e25249be94dfdfe1a908",
+    "url": "/DmitriyKulikovskiy/MyBookStore/static/media/bg1.2fd1a3cd.jpg"
+  },
+  {
+    "revision": "c1df47c00c375032edd90b883a44ff3f",
+    "url": "/DmitriyKulikovskiy/MyBookStore/static/media/book-page1.c1df47c0.jpg"
+  },
+  {
+    "revision": "865c8827e4f1bc8d09ce35c5d7a1fb9c",
+    "url": "/DmitriyKulikovskiy/MyBookStore/static/media/cart_title.865c8827.jpg"
+  },
+  {
+    "revision": "89ffe399970455f45879974f24621e2b",
+    "url": "/DmitriyKulikovskiy/MyBookStore/static/media/client1.89ffe399.png"
+  },
+  {
+    "revision": "fbcb06f8001bb01df5927293972dfcdc",
+    "url": "/DmitriyKulikovskiy/MyBookStore/static/media/client2.fbcb06f8.png"
+  },
+  {
+    "revision": "382d14c1273d0941bfb6e65a0ddc2940",
+    "url": "/DmitriyKulikovskiy/MyBookStore/static/media/client3.382d14c1.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/DmitriyKulikovskiy/MyBookStore/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/DmitriyKulikovskiy/MyBookStore/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/DmitriyKulikovskiy/MyBookStore/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "66ba18c517770cd5a32cb6d29843839e",
+    "url": "/DmitriyKulikovskiy/MyBookStore/static/media/woman-bg.66ba18c5.jpg"
+  }
+]);
